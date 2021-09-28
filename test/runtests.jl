2b0aac2e2a0bdd92318179dcc2b30ddf8a9ccf7e
Niklas Heim <heimnikl@fel.cvut.cz>
2021-09-28 22:41:54 +0200

    fix world constructor; add some tests (#7)

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,16 @@ EcosystemCore.mates(a::Animal{S},b::Animal{S}) where S<:Species = true
 EcosystemCore.mates(a::Agent, b::Agent) = false
 
 @testset "EcosystemCore" begin
+    g = Grass(1,10)
+    @test_throws ErrorException World([g,g])
+
+    g = Grass(1,1,1)
+    s = Animal{Sheep,Male}(2,1,1,1,1)
+    w = Animal{Wolf,Female}(3,1,1,1,1)
+    @test repr(g) == "🌿  #1 100% grown"
+    @test repr(s) == "🐑♂ #2 E=1.0 ΔE=1.0 pr=1.0 pf=1.0"
+    @test repr(w) == "🐺♀ #3 E=1.0 ΔE=1.0 pr=1.0 pf=1.0"
+    @test_nowarn repr(World([g,s,w]))
 
     grass1 = Grass(1,1,2)
     grass2 = Grass(2,2,2)
